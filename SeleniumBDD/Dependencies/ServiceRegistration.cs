04b19497fb42323acfb7338383cd28c25d56035b
Jakub Koścień <jakub.koscien@gmail.com>
2026-02-06 01:57:31 +0100

    Add service registration class for DI purposes compatible with Reqnroll

---
+++ SeleniumBDD/Dependencies/ServiceRegistration.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using OpenQA.Selenium;
+using Reqnroll.Microsoft.Extensions.DependencyInjection;
+using SeleniumBDD.Config;
+using SeleniumBDD.Drivers;
+using SeleniumBDD.Pages;
+
+namespace SeleniumBDD.Dependencies
+{
+    public class ServiceRegistration
+    {
+        [ScenarioDependencies]
+        public static IServiceCollection CreateServices()
+        {
+            var services = new ServiceCollection();
+
+            // Read appsettings.json
+            var config = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            // Register TestSettings
+            var testSettings = config.Get<TestSettings>();
+            services.AddSingleton(testSettings);
+
+            // Driver Factory
+            services.AddScoped<IDriverFactory, DriverFactory>();
+
+            // WebDriver
+            services.AddScoped<IWebDriver>(provider =>
+            {
+                var factory = provider.GetRequiredService<IDriverFactory>();
+                return factory.Create();
+            });
+
+            // Pages
+            services.AddScoped<LoginPage>();
+
+            return services;
+        }
+    }
+}
