60266a3f9a77f8a5c849d2b9dabe6861659d3c3a
Jakub Koścień <jakub.koscien@gmail.com>
2026-02-06 01:56:41 +0100

    Add configuration json and TestSettings model class

---
+++ SeleniumBDD/Config/TestSettings.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace SeleniumBDD.Config
+{
+    public class TestSettings
+    {
+        public string BaseUrl { get; set; }
+        public string Browser { get; set; }
+    }
+}
