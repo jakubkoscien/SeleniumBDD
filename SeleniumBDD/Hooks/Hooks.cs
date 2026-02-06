25426ba6a2a5a69632d7049bf5403a51c777480c
Jakub Koścień <jakub.koscien@gmail.com>
2026-02-06 22:21:58 +0100

    Add Hooks class

---
+++ SeleniumBDD/Hooks/Hooks.cs
@@ -0,0 +1,21 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using Reqnroll;
+using Reqnroll.BoDi;
+using SeleniumBDD.Config;
+using System.Text.Json;
+using Microsoft.Extensions.DependencyInjection;
+using Reqnroll.Microsoft.Extensions.DependencyInjection;
+namespace SeleniumBDD.DI
+{
+	[Binding]
+	public class Hooks
+	{
+        [AfterScenario]
+        public void Cleanup(IWebDriver driver)
+        {
+            driver.Quit();
+        }
+
+    }
+}
