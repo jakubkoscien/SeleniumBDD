3ce7d0eed67d6cffdc42983e7e54de8618036a0e
Jakub Koścień <jakub.koscien@gmail.com>
2026-02-13 01:04:03 +0100

    Add registration page required classes

---
+++ SeleniumBDD/Pages/RegistrationPage.cs
@@ -0,0 +1,25 @@
+﻿using OpenQA.Selenium;
+using SeleniumBDD.Config;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SeleniumBDD.Pages;
+public class RegistrationPage : BasePage
+{
+    private readonly TestSettings _settings;
+    public RegistrationPage(IWebDriver driver, TestSettings settings) : base(driver) 
+    {   
+        _settings = settings;
+    }
+
+    private readonly By _emailInput = By.Id("");
+    private readonly By _passwordInput = By.Id("");
+    private readonly By _confirmPasswordInput = By.Id("");
+    private readonly By _registerButton = By.CssSelector("");
+    private readonly By _signInButton = By.LinkText("");
+    private readonly By _backToLoginPageButton = By.LinkText("");
+
+    public void GoToRegistrationPage() => GoToUrl(_settings.BaseUrl + "register");
+
+}
---
+++ SeleniumBDD/Steps/RegistrationSteps.cs
@@ -0,0 +1,15 @@
+﻿using SeleniumBDD.Pages;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks.Sources;
+
+namespace SeleniumBDD.Steps;
+public class RegistrationSteps
+{
+    private readonly RegistrationPage _registrationPage;
+    public RegistrationSteps(RegistrationPage registrationPage)
+    {
+        _registrationPage = registrationPage;
+    }
+}
